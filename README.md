b10801004d1c5cf14b41c3566efe4d128e178f7f
jereconjota <jeremiasfigueroa69@gmail.com>
2023-05-17 23:08:59 -0300

    Add Zustand state management library to mini React app for centralized state management, and create a counter store with initial state values and functions to increment, retrieve data, reset store, and multiply the count

---
+++ README.md
@@ -0,0 +1,3 @@
+## useZustand
+
+Mini app para el testeo del uso de la librería [zustand](https://zustand-demo.pmnd.rs/) para el manejo de un estado centralizado en React.
--- src/store/counterStore.ts
+++ src/store/counterStore.ts
@@ -1,5 +1,6 @@
 import create from "zustand";
 
+// Define a TypeScript interfaces
 export interface Post {
   id: number;
   title: string;
@@ -10,27 +11,36 @@ interface CounterState {
   count: number;
   title: string;
   posts: Post[];
-  increment: (value: number) => void;
-  getPosts: () => Promise<void>;
-  cleanStore: () => void;
-  multiply: (value: number) => void;
+  increment: (value: number) => void; // A function that takes a number and returns nothing
+  getPosts: () => Promise<void>; // A function that returns a Promise of nothing
+  cleanStore: () => void; // A function that returns nothing
+  multiply: (value: number) => void; // A function that takes a number and returns nothing
 }
 
+// Create the counter store using Zustand
 export const useCounterStore = create<CounterState>((set, get) => ({
+  // Set some initial state values
   title: "Some title",
   count: 10,
   posts: [],
+
+  // Takes a value and adds it to the count
   increment: (value: number) =>
     set((state) => ({ ...state, count: state.count + value })),
+
+  // Retrieves data from an external JSON API
   getPosts: async () => {
     const posts = await (
       await fetch("https://jsonplaceholder.typicode.com/posts")
     ).json();
     set((state) => ({ ...state, posts }));
   },
+
+  // Resets the store to its initial state
   cleanStore: () => set({}, true),
+
+  // Multiplies the count by a value
   multiply: (value: number) => {
-    // const count = get().count
     const { count } = get();
     set({ count: count * value });
   },
